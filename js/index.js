a6940b1ce86bfe7a3734ced44a03bdd059230621
johndoe@example.com <johndoe@example.com>
2022-09-21 08:37:11 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,33 @@
-// Iteration 1: Names and Input
-hola
+let hacker1 = "Raúl"
+console.log(`The driver's name is ${hacker1}`);
 
-// Iteration 2: Conditionals
+let hacker2 = "Claudia"
+console.log(`The name navigator's ${hacker2}`);
 
 
-// Iteration 3: Loops
+
+if(hacker1.length > hacker2.length){ 
+    console.log(`The driver has the longest name, it has 4 characters ${hacker1.length} caracteres`); 
+  } 
+  else  if(hacker1.length < hacker2.length){ 
+    console.log( `It seems that the navigator has the longest name, it has 5 ${hacker2.length}caracteres`); 
+  } 
+else{ 
+    console.log(`Wow, you both have equally long names, XX characters!.`); 
+  }
+
+
+let driverName = "";
+let secondDriver = "john"
+   for(let i = o; < secondDriver.length; i ++){
+    driverName += secondDriver[i].toUpperCase() " "
+   }
+
+let navigatorName = ""
+let secondNavName = "nhoJ"
+for(let i = secondNavName.length - 1; i >= 0; i --){
+  navigatorName += secondDriver[i];
+}
+console.log(navigatorName);
+
+
